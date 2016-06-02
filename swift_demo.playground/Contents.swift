8f8f0675ba530e2e24b7ca1389a9b5671139e342
FighterRay27 <554269861@qq.com>
2016-06-02 22:49:08 +0800

    下标Subscript

--- swift_demo.playground/Contents.swift
+++ swift_demo.playground/Contents.swift
@@ -476,6 +476,54 @@ if player.tracker.advenceToLevel(7) {
     print("关卡7暂时还没有解锁")
 }
 
+//下标 Subscript
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+
+let threeTimesTable = TimesTable(multiplier: 3)
+print("six times three is \(threeTimesTable[6])")
+
+//使用下标语法创建一个矩阵
+struct Matrix {
+    
+    let rows: Int, columns: Int
+    var grid: [Double]
+    
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(count: rows * columns, repeatedValue:0.0)
+    }
+    
+    func indexIsValidForRow(row: Int, column:Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+}
+
+var matrix = Matrix(rows: 2, columns: 2)
+matrix[0, 1] = 1.5
+matrix[1, 0] = 3.2
+
+
+
+
+
+
 
 
 
