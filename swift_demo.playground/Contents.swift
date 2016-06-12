7f1bb70daea2f4b2a9257b6fcc28da5c6e819457
FighterRay27 <554269861@qq.com>
2016-06-12 11:40:08 +0800

    自动引用计数
    解决因循环强引用导致的内存泄漏问题

--- swift_demo.playground/Contents.swift
+++ swift_demo.playground/Contents.swift
@@ -947,10 +947,147 @@ bankPlayer = nil
 print("BankPlayer has left the game, the bank now has \(Bank.coinsInBank) coins")
 
 
+/**
+ *  自动引用计数 Automatic Reference Counting
+ */
 
+class Person {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+        print("\(name) is being initialized")
+    }
+    
+    var apartment: Apartment?
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+class Apartment {
+    let unit: String
+    init(unit: String) {
+        self.unit = unit
+    }
+    weak var tenant: Person? //使用弱引用
+    deinit {
+        print("Apartment \(unit) is being deinitialized")
+    }
+}
 
+//定义3个可选的Person类型的变量, 自动初始化为nil
+var reference1: Person?
+var reference2: Person?
+var reference3: Person?
 
+reference1 = Person(name: "John Appleseed")
+reference2 = reference1
+reference3 = reference1
 
+reference1 = nil
+reference2 = nil
+reference3 = nil
+
+//类实例之间的循环强引用♻️ Strong Reference Cycle
+
+var john: Person?
+var unit4A: Apartment?
+
+john = Person(name: "FighterRay")
+unit4A = Apartment(unit: "4A")
+
+john!.apartment = unit4A
+unit4A!.tenant = john
+
+john = nil
+unit4A = nil//不会调用这两个变量的析构器
+
+//对于生命周期中会变为nil的实例使用 弱引用 (只能用于可选类型的变量) weak reference
+//对于初始化赋值后再也不会被赋值为nil的实例使用 无主引用 (用于非可选类型的常量或变量) unowned reference
+class Customer {
+    let name: String
+    var card: CreditCard?
+    init(name: String) {
+        self.name = name
+    }
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+
+class CreditCard {
+    let number: UInt64
+    unowned let customer: Customer
+    init(number: UInt64, customer: Customer) {
+        self.number = number
+        self.customer = customer
+    }
+    deinit {
+        print("Card #\(number) is being deinitialized")
+    }
+}
+
+var ray: Customer? = Customer(name: "ray")
+ray?.card = CreditCard(number: 1234_5678_9012_3456, customer: ray!)
+ray = nil
+
+//无主引用 和 隐式解析可选属性 unowned reference and implicitly unwrapped optional properties
+class Country {
+    let name: String
+    var capitalCity: City!
+    init(name: String, capitalName: String) {
+        self.name = name
+        self.capitalCity = City(name: capitalName, country: self)
+    }
+}
+
+class City {
+    let name: String
+    unowned let country: Country
+    init(name: String, country: Country) {
+        self.name = name
+        self.country = country
+    }
+}
+
+var country = Country(name: "Canada", capitalName: "Ottawa")
+print("\(country.name)'s capital city is called \(country.capitalCity.name)")
+
+//闭包引起的循环强引用
+//闭包捕获列表 closure capture list
+class HTMLElement {
+    let name: String
+    let text: String?
+    
+    lazy var asHTML: Void -> String = {
+        [unowned self] in
+        if let text = self.text {
+            return "<\(self.name)> \(text) </\(self.name)>"
+        } else {
+            return "<\(self.name) />"
+        }
+    }
+    
+    init(name: String, text: String? = nil) {
+        self.name = name
+        self.text = text
+    }
+    
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+
+let heading = HTMLElement(name: "h1")
+let defaultText = "some default text"
+heading.asHTML = {
+    return "<\(heading.name)\(heading.text ?? defaultText)</\(heading.name)>"
+}
+print(heading.asHTML())
+
+var paragraph: HTMLElement? = HTMLElement(name: "p", text: "hello world")
+print(paragraph!.asHTML())
+paragraph = nil
 
 
 
