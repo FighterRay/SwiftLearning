2c9eb9a8f71c9ae55175439390f6ec94cec7716a
FighterRay27 <554269861@qq.com>
2016-06-15 13:30:44 +0800

    可选链式调用

--- swift_demo.playground/Contents.swift
+++ swift_demo.playground/Contents.swift
@@ -1090,6 +1090,126 @@ print(paragraph!.asHTML())
 paragraph = nil
 
 
+/**
+ *  可选链式调用 Optional Chaining
+ */
+class People {
+    var residence: Residence?
+}
+
+class Residence {
+    var rooms = [Room]();
+    var numberOfRooms: Int {
+        return rooms.count
+    }
+    
+    subscript(i: Int) -> Room {
+        get {
+            return rooms[i]
+        }
+        set {
+            rooms[i] = newValue
+        }
+    }
+    
+    func printNumberOfRooms() {
+        print("The number of rooms is \(numberOfRooms)")
+    }
+    
+    var address: Address?
+}
+
+class Room {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Address {
+    var buildingName: String?
+    var buildingNumber: String?
+    var street: String?
+    
+    func buildingIdentifier() -> String? {
+        if buildingName != nil {
+            return buildingName
+        } else if buildingNumber != nil && street != nil {
+            return "\(buildingName) \(street)"
+        } else {
+            return nil
+        }
+    }
+}
+
+//通过可选链式调用来访问属性值, 在一个可选值上访问它的属性，并判断访问是否成功
+let bob = People()
+if let roomCount = bob.residence?.numberOfRooms {
+    print("Bob's residence has \(roomCount) rooms")
+} else {
+    print("Unable to retrieve the number of rooms")
+}
+
+func creatAddress() -> Address {
+    print("Function was called.")
+    
+    let someAddress = Address()
+    someAddress.buildingNumber = "29"
+    someAddress.street = "Acacia Road"
+    
+    return someAddress
+}
+
+//通过可选链式调用来设置属性值
+//可选链式调用失败时，等号右侧的代码不会被执行
+
+bob.residence?.address = creatAddress()
+
+//通过可选链式调用调用方法
+//如果在可选值上通过可选链式调用来调用这个方法，该方法的返回类型会是Void?，而不是Void，因为通过可选链式调用得到的返回值都是可选的
+if bob.residence?.printNumberOfRooms() != nil {
+    print("It was possible to print the number of rooms.")
+} else {
+    print("It was not possible to print the number of rooms.")
+}
+
+//通过可选链式调用访问下标
+if let firstRoomName = bob.residence?[0].name {
+    print("The first room name is \(firstRoomName).")
+} else {
+    print("Unable to retrieve the first room name.")
+}
+
+//以下赋值会失败，因为 residence 目前是 nil
+bob.residence?[0] = Room(name: "Bathroom")
+
+//访问可选类型的下标
+var testScores = ["Dave": [1, 2, 3], "Bev": [4, 5 ,6]]
+testScores["Dave"]?[0] = 91 //调用成功
+testScores["Bev"]?[0] += 1 //调用成功
+testScores["Brian"]?[0] = 72 //调用失败
+
+//链接多层可选链式调用
+//如果你访问的值不是可选的，可选链式调用将会返回可选值。如果你访问的值就是可选的，可选链式调用不会让可选返回值变得“更可选”。
+if let johnsStreet = bob.residence?.address?.street {
+    print("John's street name is \(johnsStreet).")
+} else {
+    print("Unable to retrieve the address.")
+}
+
+//在方法的可选返回值上进行可选链式调用
+if let beginsWithThe =
+    bob.residence?.address?.buildingIdentifier()?.hasPrefix("The") {
+        if beginsWithThe {
+            print("Bob's building identifer begins with \"The\".")
+        } else {
+            print("Bob's building identifer does not begin with \"The\".")
+        }
+}
+
+
+
+
 
 
 
