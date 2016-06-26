a1b260b5760600d7ce806f5bba8459e8bc731b54
FighterRay27 <554269861@qq.com>
2016-06-26 23:48:13 +0800

    嵌套类型 Nesting Types

--- swift_demo.playground/Contents.swift
+++ swift_demo.playground/Contents.swift
@@ -1407,7 +1407,44 @@ for thing in things {
 }
 
 
-
+/**
+ *  嵌套类型 Nested
+ */
+struct BlackjackCard {
+    //嵌套的 Suit 枚举
+    enum Suit: Character {
+        case Spades = "♠︎", Hearts = "♡", Diamonds = "♢", Clubs = "♣︎"
+    }
+    
+    //嵌套的 Rank 枚举
+    enum Rank: Int {
+        case Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+        case Jack, Queen, King, Ace
+        struct Values {
+            let first: Int, second: Int?
+        }
+        var values: Values {
+            switch self {
+            case .Ace:
+                return Values(first: 1, second: 11)
+            case .Jack, .Queen, .King:
+                return Values(first: 10, second: nil)
+            default:
+                return Values(first: self.rawValue, second: nil)
+            }
+        }
+    }
+    
+    let rank: Rank, suit: Suit
+    var description: String {
+        var output = "suit is \(suit.rawValue),"
+        output += " value is \(rank.values.first)"
+        if let second = rank.values.second {
+            output += " or \(second)"
+        }
+        return output
+    }
+}
 
 
 
