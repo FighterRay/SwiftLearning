ee2b132e53bcbe12c77d2b3bc213cb367c63c423
FighterRay27 <554269861@qq.com>
2016-07-16 19:28:24 +0800

    高级运算符 Advanced Operator

--- swift_demo.playground/Contents.swift
+++ swift_demo.playground/Contents.swift
@@ -2034,12 +2034,111 @@ stringToEdit.numberOfEdits
 
 
 
+/**
+ *  高级运算符  Advanced Operator
+ */
+//位运算符
+//按位取反运算符
+let initialBits: UInt8 = 0b00001111
+let invertedBits  = ~initialBits
 
+//按位与运算符
+let firstSixBits: UInt8 = 0b11111100
+let lastSixBits: UInt8 = 0b00111111
+let middleFourBits = firstSixBits & lastSixBits
 
+//按位或运算符
+let someBits: UInt8 = 0b10110010
+let moreBits: UInt8 = 0b01011110
+let combinedbits = someBits | moreBits
 
+//按位异或运算符
+let firstBits: UInt8 = 0b00010100
+let otherBits: UInt8 = 0b00000101
+let outputBits = firstBits ^ otherBits // 等于 00010001
 
+//无符号整数的移动位运算
+let shiftBits: UInt8 = 4 //即二进制的 00000100
+shiftBits << 1
+shiftBits << 2
 
+//有符号整数的移位运算
+let pink: UInt32 = 0xCC6699
+let redComponent = (pink & 0xFF0000) >> 16
+let greenComponent = (pink & 0x00FF00) >> 8
+let blueComponent = pink & 0x0000FF
 
+//溢出运算符： 
+//对于无符号与有符号整型数值来说，当出现上溢时，它们会从数值所能容纳的最大数变成最小的数。
+//同样地，当发生下溢时，它们会从所能容纳的最小数变成最大的数。
+
+var unsignedOverflow = UInt8.max
+// unsignedOverflow 等于 UInt8 所能容纳的最大整数 255
+unsignedOverflow = unsignedOverflow &+ 1
+// 此时 unsignedOverflow 等于 0
+
+//运算符函数
+struct Vector2D {
+    var x = 0.0, y = 0.0
+}
+func + (left: Vector2D, right: Vector2D) -> Vector2D {
+    return Vector2D(x: left.x + right.x, y: left.y + right.y)
+}
+
+let vector = Vector2D(x: 3.0, y: 1.0)
+let anotherVector = Vector2D(x: 2.0, y: 4.0)
+let combinedVector = vector + anotherVector
+
+//前缀和后缀运算符
+prefix func - (vector: Vector2D) -> Vector2D {
+    return Vector2D(x: -vector.x, y: -vector.y)
+}
+
+//复合赋值运算符
+func += (inout left: Vector2D, right: Vector2D) {
+    left = left + right
+}
+var original = Vector2D(x: 1.0, y: 2.0)
+let vectorToAdd = Vector2D(x: 3.0, y: 4.0)
+original += vectorToAdd
+
+prefix func ++ (inout vector: Vector2D) -> Vector2D {
+    vector += Vector2D(x: 1.0, y: 1.0)
+    return vector
+}
+var toIncrement = Vector2D(x: 3.0, y: 4.0)
+let afterIncrement = ++toIncrement
+
+//等价运算符
+func == (left: Vector2D, right: Vector2D) -> Bool{
+    return (left.x == right.x) && (left.y == right.y)
+}
+func != (left: Vector2D, right: Vector2D) -> Bool {
+    return !(left == right)
+}
+let twoThree = Vector2D(x: 2.0, y: 3.0)
+let anotherTwoThree = Vector2D(x: 2.0, y: 3.0)
+if twoThree == anotherTwoThree {
+    print("These two vectors are equivalent.")
+}
+
+//自定义运算符
+prefix operator +++ {}
+prefix func +++ (inout vector: Vector2D) -> Vector2D {
+    vector += vector
+    return vector
+}
+var toBeDoubled = Vector2D(x: 1.0, y: 4.0)
+let afterDoubling = +++toBeDoubled
+
+//自定义中缀运算符的优先级和结合性
+infix operator +- { associativity left precedence 140}
+func +- (left: Vector2D, right: Vector2D) -> Vector2D{
+    return Vector2D(x: left.x + right.x, y: left.y - right.y)
+}
+let firstVector = Vector2D(x: 1.0, y: 2.0)
+let secondVector = Vector2D(x: 3.0, y: 4.0)
+let plusMinusVector = firstVector +- secondVector
 
 
 
